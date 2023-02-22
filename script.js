93099d77f187d04e7c51ccdf2769d1018cf186cb
aya-no <121071722+aya-no@users.noreply.github.com>
2023-02-22 16:44:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -93,8 +93,191 @@
     url:`http://umapanera.com/?pid=111054790`,
     score: 0,
   };
+
+  const columbia = {
+    bitterness: 2,
+    acidity: 2,
+    sweets: 4,
+    richBody: 2,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "コロンビア（ピコクリストバル）",
+    about: "完熟したコーヒーチェリーのみ手摘みした甘味と苦味のバランスやマイルド感が素晴らしいスペシャルティ豆です。",
+    yen: "税込 1,512円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50142123`,
+    score: 0,
+  };
+
+  const blendFufu = {
+    bitterness: 2,
+    acidity: 4,
+    sweets: 4,
+    richBody: 4,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "夫婦（ブレンド）",
+    about: "キリマンジャロ豆ベースの落ち着いた味わいが夜のひと時にピッタリ。",
+    yen: "税込 1,447円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50142693`,
+    score: 0,
+  };
+
+  const blendEgao = {
+    bitterness: 2,
+    acidity: 2,
+    sweets: 4,
+    richBody: 3,
+    flavor: 5,
+    highFrequently: false,
+    beansName: "笑顔（ブレンド）",
+    about: "モカ豆ベースの独特で深い香りがゆったりとした気持ちにしてくれる。",
+    yen: "税込 1,447円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50142860`,
+    score: 0,
+  };
+
+  const blendKenko = {
+    bitterness: 2,
+    acidity: 2,
+    sweets: 2,
+    richBody: 3,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "健康（ブレンド）",
+    about: "ブラジル豆ベースの苦味・酸味・甘味のバランスが良く、ガブガブ飲めます。",
+    yen: "税込 1,534円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50142944`,
+    score: 0,
+  };
+
+  const blendKazoku = {
+    bitterness: 3,
+    acidity: 3,
+    sweets: 2,
+    richBody: 3,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "家族（ブレンド）",
+    about: "苦味と甘味のバランスが良く、安定感のあるマイルドな味わいを楽しめる。",
+    yen: "税込 1,404円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50142996`,
+    score: 0,
+  };
+
+  const blendAya = {
+    bitterness: 4,
+    acidity: 2,
+    sweets: 3,
+    richBody: 4,
+    flavor: 5,
+    highFrequently: false,
+    beansName: "綾（ブレンド）",
+    about: "トラジャ中心のやわらかい酸味と高貴な香りが特徴。まろやかで飲みやすい珈琲。",
+    yen: "税込 1,620円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50528537`,
+    score: 0,
+  };
   
-  const coffeeBeansList = [mocha,toraja,kilimanjaro,blendEn,columbiaDecaf,BrazilDecaf];
+  const blendMiyabi = {
+    bitterness: 2,
+    acidity: 2,
+    sweets: 3,
+    richBody: 3,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "雅（ブレンド）",
+    about: "落ち着きのある香りと深いコクは、食事の後やホッとしたい時にピッタリ。",
+    yen: "税込 1,447円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=50528595`,
+    score: 0,
+  };
+
+  const guatemala = {
+    bitterness: 3,
+    acidity: 4,
+    sweets: 4,
+    richBody: 3,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "グァテマラ（サンタバーバラ）",
+    about: "過酷ともいえる標高で生育されるためわずかしか生産できません。逆境を越えたものだけが到達する真のコーヒーです。",
+    yen: "税込 1,620円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=55770095`,
+    score: 0,
+  };
+
+  const amarellobourbon = {
+    bitterness: 3,
+    acidity: 4,
+    sweets: 4,
+    richBody: 4,
+    flavor: 5,
+    highFrequently: false,
+    beansName: "アマレロブルボン",
+    about: "アマレロは収穫量が極めて少ないブルボン種の中でもさらに希少な存在。UTZ認証も与えられた最高級豆。",
+    yen: "税込 1,512円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=57806993`,
+    score: 0,
+  };
+
+  const ethiopiaDecaf = {
+    bitterness: 2,
+    acidity: 3,
+    sweets: 3,
+    richBody: 2,
+    flavor: 4,
+    highFrequently: true,
+    beansName: "エチオビア・デカフェ",
+    about: "カフェイン残留率を0.1%以下に抑えた画期的な製法なので、妊産婦・授乳中の方、カフェインで眠れなくなる方、健康志向の方にお勧めできます。",
+    yen: "税込 1,836円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=111054756`,
+    score: 0,
+  };
+
+  const chocolate = {
+    bitterness: 3,
+    acidity: 2,
+    sweets: 4,
+    richBody: 4,
+    flavor: 4,
+    highFrequently: false,
+    beansName: "ブラジル・ショコラ",
+    about: "ビターチョコのような重厚感のある甘味を楽しめます。酸味の苦手な方へ特にオススメです。",
+    yen: "税込 1,620円/200g",
+    img: "beans.jpeg",
+    url:`http://umapanera.com/?pid=133804987`,
+    score: 0,
+  };
+
+  const coffeeBeansList = [
+    mocha,
+    toraja,
+    kilimanjaro,
+    blendEn,
+    columbiaDecaf,
+    BrazilDecaf,
+    columbia,
+    blendFufu,
+    blendEgao,
+    blendKenko,
+    blendKazoku,
+    blendAya,
+    blendMiyabi,
+    guatemala,
+    amarellobourbon,
+    ethiopiaDecaf,
+    chocolate
+  ];
 
 
 // // スライダーバーの値をリアルタイムに表示//////////////////////
@@ -144,7 +327,7 @@ function suggestClick(){
     selectBeans(select); //豆ごとにスコア付け&並び替え関数を実行  
     suggestBeans(coffeeBeansList);//１～３位のおすすめお豆関数実行
     no1BeansChart(coffeeBeansList)//１位のお豆のレーダーチャート作成関数実行
-    console.log(coffeeBeansList);
+    // console.log(coffeeBeansList);
 }
 
 // 豆ごとにスコア付け　////////////////////////////////////////////////////////////////
@@ -169,7 +352,7 @@ function suggestClick(){
         score++;
       }
       if ( select.highFrequently === beans.highFrequently ){
-        score++;
+        score = score + 0.1;
       }
     beans.score = score
     }
@@ -231,7 +414,7 @@ let mychart = new Chart(ctx, {
             borderWidth: 1,
             // pointBackgroundColor: 'rgb(46,106,177)',
         },{
-            label: 'おすすめ 1位 のお豆',
+            label: 'おすすめ 1位 のコーヒー',
             data: [
                 arrayOfObject[0].bitterness,
                 arrayOfObject[0].acidity,
@@ -248,7 +431,7 @@ let mychart = new Chart(ctx, {
     options: {
         title: {
             display: false,
-            text: 'オススメお豆',
+            text: 'おすすめコーヒー',
         },
         scale: {
             ticks: {
@@ -262,5 +445,13 @@ let mychart = new Chart(ctx, {
         }
     }
 });
+
+}
+
+// FND所感表示////////////////////////////////////////////////////////////
+
+function heartClick() {
+
+  document.getElementById("impression").style.display = "block";
 
 }
